8f6fbdb3d13d65327ac52e30969fce8e743c3c8f
Masonga222 <masongashemap@gmail.com>
2024-05-28 01:07:19 +0200

    0x00-ES6_basic

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
-	export default function appendToEachArrayValue(array, appendString) {
-  const newArray = [];
-  for (const value of array) {
-    newArray.push(appendString + value);
+export default function appendToEachArrayValue(array, appendString) {
+  const arrayEnd = [];
+  for (const idx of array) {
+    arrayEnd.push(`${appendString}${idx}`);
   }
 
-  return newArray;
+  return arrayEnd;
 }
